698b1c8e4878792770a3c258731b72398ba22202
millscan <33579432+millscan@users.noreply.github.com>
2018-10-18 14:51:55 -0700

    test commit

--- University_Canfield/src/org/university/software/Driver1.java
+++ University_Canfield/src/org/university/software/Driver1.java
@@ -4,6 +4,8 @@ import org.university.hardware.Classroom;
 import org.university.hardware.Department;
 import org.university.people.Professor;
 import org.university.people.Student;
+import org.university.people.Staff;
+
 
 public class Driver1 
 {
@@ -32,16 +34,22 @@ public class Driver1
 		Student s3 = new Student();
 		Student s4 = new Student();
 
-		/* Set Course */
+		/* Set Campus Course */
 
-		Course c1 = new Course();
-		Course c2 = new Course();
-		Course c3 = new Course();
-		Course c4 = new Course();
-		Course c5 = new Course();
-		Course c6 = new Course();
-		Course c7 = new Course();
-		Course c8 = new Course();
+		CampusCourse c1 = new CampusCourse() ;
+		CampusCourse c2 = new CampusCourse() ;
+		CampusCourse c3 = new CampusCourse() ;
+		CampusCourse c4 = new CampusCourse() ;
+		CampusCourse c5 = new CampusCourse() ;
+		CampusCourse c6 = new CampusCourse() ;
+		CampusCourse c7 = new CampusCourse() ;
+		CampusCourse c8 = new CampusCourse() ;
+		
+		/* Set Online Courses */
+		OnlineCourse oc1 = new OnlineCourse();
+		OnlineCourse oc2 = new OnlineCourse();
+		OnlineCourse oc3 = new OnlineCourse();
+		OnlineCourse oc4 = new OnlineCourse();
 
 		/* Set Room */
 
@@ -58,6 +66,9 @@ public class Driver1
 		Professor p4 = new Professor();
 		Professor p5 = new Professor();
 
+		/* Set Staff */
+
+		Staff sf1 = new Staff();
 
 		/* Set Attributes */
 
@@ -75,35 +86,75 @@ public class Driver1
 
 		c1.setCourseNumber(387);
 		c1.setName("Enterprise Web Applications");
-		dept2.addCourse(c1);
+		c1.setCreditUnits(3);
+		c1.setMaxCourseLimit(3);
+		dept2.addCampusCourse(c1);
 
 		c2.setCourseNumber(372);
 		c2.setName("Comparative Programming Languages");
-		dept2.addCourse(c2);
+		c2.setCreditUnits(4);
+		c2.setMaxCourseLimit(3);
+		dept2.addCampusCourse(c2);
 
 		c3.setCourseNumber(345);
 		c3.setName("Discrete Structures");
-		dept2.addCourse(c3);
+		c3.setCreditUnits(4);
+		c3.setMaxCourseLimit(3);
+		dept2.addCampusCourse(c3);
 
 		c4.setCourseNumber(426);
 		c4.setName("Computer Networks");
-		dept2.addCourse(c4);
+		c4.setCreditUnits(4);
+		c4.setMaxCourseLimit(3);
+		dept2.addCampusCourse(c4);
 
 		c5.setCourseNumber(275);
 		c5.setName("Computer Programming II");
-		dept1.addCourse(c5);
+		c5.setCreditUnits(3);
+		c5.setMaxCourseLimit(3);
+		dept1.addCampusCourse(c5);
 
 		c6.setCourseNumber(320);
 		c6.setName("Circuits Analysis");
-		dept1.addCourse(c6);
+		c6.setCreditUnits(3);
+		c6.setMaxCourseLimit(3);
+		dept1.addCampusCourse(c6);
 
 		c7.setCourseNumber(373);
 		c7.setName("Object Oriented Software Design");
-		dept1.addCourse(c7);
+		c7.setCreditUnits(3);
+		c7.setMaxCourseLimit(3);
+		dept1.addCampusCourse(c7);
 
 		c8.setCourseNumber(107);
 		c8.setName("Experimental Course");
-		dept1.addCourse(c8);
+		c8.setCreditUnits(3);
+		c8.setMaxCourseLimit(3);
+		dept1.addCampusCourse(c8);
+		
+		/* Set Online course name, number and department*/
+		oc1.setCourseNumber(610);
+		oc1.setName("Online_Course_1");
+		oc1.setCreditUnits(3);
+		dept1.addOnlineCourse(oc1);
+		
+		oc2.setCourseNumber(620);
+		oc2.setName("Online_Course_2");
+		oc2.setCreditUnits(3);
+		dept1.addOnlineCourse(oc2);
+		
+		oc3.setCourseNumber(630);
+		oc3.setName("Online_Course_3");
+		oc3.setCreditUnits(3);
+		dept2.addOnlineCourse(oc3);
+		
+		
+		oc4.setCourseNumber(640);
+		oc4.setName("Online_Course_4");
+		oc4.setCreditUnits(4);
+		dept2.addOnlineCourse(oc4);
+		
+		/////
 
 		cr1.setRoomNumber("Harvill 101");
 		cr2.setRoomNumber("Harvill 203");
@@ -120,6 +171,9 @@ public class Driver1
 		dept2.addProfessor(p4);
 		p5.setName("Homer");
 		dept2.addProfessor(p5);
+
+		sf1.setName("Carol");
+		dept2.addStaff(sf1);
 
 		univ.departmentList.add(dept1);
 		univ.departmentList.add(dept2);
@@ -154,25 +208,33 @@ public class Driver1
 
 		/* Sign up Prof. Tharp for class ECE320 */
 
-		p3.addCourse(c6);
+		p3.addCampusCourse(c6);
 
 		/* Sign up Prof. Kececioglu for class CS387 */
 
-		p4.addCourse(c1);
+		p4.addCampusCourse(c1);
 
 		/* Sign up Prof. Kececioglu for class CS372 */
 
-		p4.addCourse(c2);
+		p4.addCampusCourse(c2);
 
 		/* Sign up Prof. Homer for class CS345 */
 
-		p5.addCourse(c3);
+		p5.addCampusCourse(c3);
 
 		/* Sign up Prof. Homer for class CS426 */
 
-		p5.addCourse(c4);
+		p5.addCampusCourse(c4);
+		
+		/* Set professors for the online courses */
+		p1.addOnlineCourse(oc1);
+		p2.addOnlineCourse(oc2);
+		p4.addOnlineCourse(oc3);
+		p5.addOnlineCourse(oc4);
+		
+		///
 
-		/* Set classrooms for courses */
+		/* Set classrooms for campus courses */
 
 		c1.setRoomAssigned(cr2);
 		c2.setRoomAssigned(cr4);
@@ -182,53 +244,99 @@ public class Driver1
 
 		/* Add course to students */
 
-		s1.addCourse(c4);
-		s1.addCourse(c1);
+		s1.addCampusCourse(c4);
+		s1.addCampusCourse(c1);
 
-		s2.addCourse(c2);
-		s2.addCourse(c3);
-		s2.addCourse(c8);
+		s2.addCampusCourse(c2);
+		s2.addCampusCourse(c3);
+		s2.addCampusCourse(c8);
 
-		s3.addCourse(c1);
-		s3.addCourse(c2);
-		s3.addCourse(c3);
-		s3.addCourse(c4);
+		s3.addCampusCourse(c1);
+		s3.addCampusCourse(c2);
+		s3.addCampusCourse(c3);
+		s3.addCampusCourse(c4);
 
-		s4.addCourse(c1);
-		s4.addCourse(c2);
-		s4.addCourse(c6);
 
-		System.out.println("Testing Professor Conflicts  ...");
-		p3.addCourse(c1);
-		p4.addCourse(c3);
+		s4.addCampusCourse(c1);
+		s4.addCampusCourse(c2);
+		s4.addCampusCourse(c6);
+		
+		/* Adding online courses to students */
 
-		System.out.println("\nTesting dropCourse method in Student class ...");
-		s1.dropCourse(c7);
-		s1.addCourse(c5);
-		s1.dropCourse(c5);
-		s2.dropCourse(c8);
+		s1.addOnlineCourse(oc1);
+		s2.addOnlineCourse(oc2);
+		s3.addOnlineCourse(oc3);
+		s4.addOnlineCourse(oc4);
+		////
+		System.out.println("Testing addCampusCourse method when Campus Course has enough students  ...");
+		s2.addCampusCourse(c1);
+		
+		System.out.println("\nTesting availableTo method in CampusCourse when Campus Course has enough students  ...");
+		System.out.println("Does this Campus Course available for more student to add? True or false:");
+		System.out.println(c1.availableTo(s2));
+		
+		System.out.println("\nTesting Professor Conflicts  ...");
+		p3.addCampusCourse(c1);
+		p4.addCampusCourse(c3);
+		
+		//Checking online course conflict between professors
+		p3.addOnlineCourse(oc1);
+
+		System.out.println("\nTesting dropCampusCourse method in Student class ...");
+		s1.dropCampusCourse(c7);
+		s1.addCampusCourse(c5);
+		s1.dropCampusCourse(c5);
+		s2.dropCampusCourse(c8);
+		System.out.println("\nTesting dropCampusCourse method if campus course won't have at least 6 credts to stay enroll in online courses in Student class ...");
+		s1.dropCampusCourse(c1);
+		
+		System.out.println("\nTesting addOnlineCourse method in Student class ...");
+		s1.dropOnlineCourse(oc1);
+		s1.dropCampusCourse(c1);
+		s1.addOnlineCourse(oc2);
+		
+		System.out.println("\nTesting dropOnlineCourse method in Student class ...");
+		s1.addCampusCourse(c1);
+		s1.addOnlineCourse(oc1);
+		s1.addOnlineCourse(oc2);
+		s1.dropOnlineCourse(oc2);
+		
+		s1.dropOnlineCourse(oc3);
+		s3.dropOnlineCourse(oc1);
 
 		/* Test conflicts of student schedule */
 		System.out.println("\nConflicts:");
 
 		/* Test conflicts of classroom schedule */
+		System.out.println("\nTest conflicts of classroom schedule:");
 		c8.setRoomAssigned(cr1);
 
 		
 		/* Test conflicts of professor schedule */
-
-		p3.addCourse(c8);
-		p5.addCourse(c5);
+		System.out.println("\nTest conflicts of professor schedule:");
+		p3.addCampusCourse(c8);
+		p5.addCampusCourse(c5);
 
 		/* Try to add ECE320 to student Lahiru */
-
-		s1.addCourse(c6);
+		System.out.println("\ntest Conflicts when add course to student:");
+		s1.addCampusCourse(c6);
 
 		/* Try to add ECE373 to student Lahiru */
 
-		s1.addCourse(c7);
+		s1.addCampusCourse(c7);
 
-		s2.addCourse(c5);
+		s2.addCampusCourse(c5);
+
+		/* Add course to staff */
+		System.out.println("\ntest Conflicts when add course to staff:");
+		sf1.addCampusCourse(c1);
+
+		/* Test adding more than 1 course */
+
+		sf1.addCampusCourse(c2);
+		
+		sf1.addOnlineCourse(oc3);
+		sf1.addOnlineCourse(oc4);
 
 		
 		/* Schedules */
@@ -266,18 +374,49 @@ public class Driver1
 
 		System.out.println("\nThe schedule for Student " + univ.departmentList.get(0).getStudentList().get(0).getName() + ":");
 		univ.departmentList.get(0).getStudentList().get(0).printSchedule();
-
+		
+		
 		System.out.println("\nThe schedule for Student " + univ.departmentList.get(1).getStudentList().get(0).getName() + ":");
 		univ.departmentList.get(1).getStudentList().get(0).printSchedule();
 
+		
 		System.out.println("\nThe schedule for Student " + univ.departmentList.get(1).getStudentList().get(1).getName() + ":");
 		univ.departmentList.get(1).getStudentList().get(1).printSchedule();
 
+		
+		/* Staff schedules */
 
-		/* Print schedule of classroom 4 */
+		System.out.println("\nThe schedule for Employee " + univ.departmentList.get(1).getStaffList().get(0).getName() + ":");
+		univ.departmentList.get(1).getStaffList().get(0).printSchedule();
+		
+		/* Calculate tuition fees */
+		
+		System.out.println("\nTuition Fee for student " + univ.departmentList.get(0).getStudentList().get(0).getName() + " is " + 
+				String.format("%d", univ.departmentList.get(0).getStudentList().get(0).getTuitionFee()) );
 
-		System.out.println("\nThe schedule for course " + univ.departmentList.get(1).getCourseList().get(0).getName() + ":");
-		univ.departmentList.get(1).getCourseList().get(0).printSchedule();
+		System.out.println("Tuition Fee for student " + univ.departmentList.get(1).getStudentList().get(0).getName() + " is " + 
+				String.format("%d", univ.departmentList.get(1).getStudentList().get(0).getTuitionFee()) );
+		
+		System.out.println("Tuition Fee for student " + univ.departmentList.get(1).getStudentList().get(1).getName() + " is " + 
+				String.format("%d", univ.departmentList.get(1).getStudentList().get(1).getTuitionFee()) );
+		
+		System.out.println("Tuition Fee for Employee  " + univ.departmentList.get(1).getStaffList().get(0).getName() + " is " + 
+				String.format("%d", univ.departmentList.get(1).getStaffList().get(0).getTuitionFee()));
+		
+		/* Print schedule of course 1 */
+
+		System.out.println("\nThe schedule for course " + univ.departmentList.get(1).getCampusCourseList().get(0).getName() + ":");
+		univ.departmentList.get(1).getCampusCourseList().get(0).printSchedule();
+		
+		/* Print Course List for Department ECE*/
+		System.out.println("\nThe course list for department " + univ.departmentList.get(0).getDepartmentName() + ":");
+		univ.departmentList.get(0).printCourseList();
+		
+		/*Print Course list of university*/
+		System.out.println("\nThe list of courses in the university:");
+		univ.printCourseList();
+		
+		
 
 	}
 }
--- University_Canfield/src/org/university/software/Driver2.java
+++ University_Canfield/src/org/university/software/Driver2.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 
 import org.university.hardware.Classroom;
 import org.university.hardware.Department;
+import org.university.people.Person;
+import org.university.people.Staff;
 import org.university.people.Professor;
 import org.university.people.Student;
+
 
 public class Driver2 
 {
@@ -40,14 +43,14 @@ public class Driver2
 
 		/* Set Course */
 
-		Course c1 = new Course();
-		Course c2 = new Course();
-		Course c3 = new Course();
-		Course c4 = new Course();
-		Course c5 = new Course();
-		Course c6 = new Course();
-		Course c7 = new Course();
-		Course c8 = new Course();
+		CampusCourse c1 = new CampusCourse();
+		CampusCourse c2 = new CampusCourse();
+		CampusCourse c3 = new CampusCourse();
+		CampusCourse c4 = new CampusCourse();
+		CampusCourse c5 = new CampusCourse();
+		CampusCourse c6 = new CampusCourse();
+		CampusCourse c7 = new CampusCourse();
+		CampusCourse c8 = new CampusCourse();
 
 		/* Set Room */
 
@@ -64,6 +67,9 @@ public class Driver2
 		Professor p4 = new Professor();
 		Professor p5 = new Professor();
 
+		/* Set Staff */
+
+		Staff sf1 = new Staff();
 
 		/* Set attributes */
 
@@ -81,35 +87,51 @@ public class Driver2
 
 		c1.setCourseNumber(387);
 		c1.setName("Enterprise Web Applications");
-		dept2.addCourse(c1);
+		c1.setCreditUnits(3);
+		c1.setMaxCourseLimit(3);
+		dept2.addCampusCourse(c1);
 
 		c2.setCourseNumber(372);
 		c2.setName("Comparative Programming Languages");
-		dept2.addCourse(c2);
+		c2.setCreditUnits(4);
+		c2.setMaxCourseLimit(3);
+		dept2.addCampusCourse(c2);
 
 		c3.setCourseNumber(345);
 		c3.setName("Discrete Structures");
-		dept2.addCourse(c3);
+		c3.setCreditUnits(4);
+		c3.setMaxCourseLimit(3);
+		dept2.addCampusCourse(c3);
 
 		c4.setCourseNumber(426);
 		c4.setName("Computer Networks");
-		dept2.addCourse(c4);
+		c4.setCreditUnits(4);
+		c4.setMaxCourseLimit(3);
+		dept2.addCampusCourse(c4);
 
 		c5.setCourseNumber(275);
 		c5.setName("Computer Programming II");
-		dept1.addCourse(c5);
+		c5.setCreditUnits(3);
+		c5.setMaxCourseLimit(3);
+		dept1.addCampusCourse(c5);
 
 		c6.setCourseNumber(320);
 		c6.setName("Circuits Analysis");
-		dept1.addCourse(c6);
+		c6.setCreditUnits(3);
+		c6.setMaxCourseLimit(3);
+		dept1.addCampusCourse(c6);
 
 		c7.setCourseNumber(373);
 		c7.setName("Object Oriented Software Design");
-		dept1.addCourse(c7);
+		c7.setCreditUnits(3);
+		c7.setMaxCourseLimit(3);
+		dept1.addCampusCourse(c7);
 
 		c8.setCourseNumber(107);
 		c8.setName("Experimental Course");
-		dept1.addCourse(c8);
+		c8.setCreditUnits(3);
+		c8.setMaxCourseLimit(3);
+		dept1.addCampusCourse(c8);
 
 		cr1.setRoomNumber("Harvill 101");
 		cr2.setRoomNumber("Harvill 203");
@@ -127,6 +149,8 @@ public class Driver2
 		p5.setName("Homer");
 		dept2.addProfessor(p5);
 
+		sf1.setName("Carol");
+		dept2.addStaff(sf1);
 
 		univ.departmentList.add(dept1);
 		univ.departmentList.add(dept2);
@@ -159,30 +183,65 @@ public class Driver2
 
 		/* Add course to students */
 
-		s1.addCourse(c4);
-		s1.addCourse(c1);
-		s1.addCourse(c2);
+		s1.addCampusCourse(c4);
+		s1.addCampusCourse(c1);
+		s1.addCampusCourse(c2);
 
-		s2.addCourse(c2);
-		s2.addCourse(c3);
+		s2.addCampusCourse(c2);
+		s2.addCampusCourse(c3);
 
-		s3.addCourse(c1);
-		s3.addCourse(c2);
-		s3.addCourse(c3);
-		s3.addCourse(c4);
+		s3.addCampusCourse(c1);
+		s3.addCampusCourse(c2);
+		s3.addCampusCourse(c3);
+		s3.addCampusCourse(c4);
 
-		s4.addCourse(c1);
-		s4.addCourse(c2);
-		s4.addCourse(c6);
+		s4.addCampusCourse(c1);
+		s4.addCampusCourse(c6);
 
+		/* Test staff pay increase */
+
+		System.out.println("Testing Pay increases  ....");
+
+		/* Rate is $ per hour */
+
+		sf1.setPayRate(20.5);
+
+		/* Hours are reported as per pay period */
+
+		sf1.setMonthlyHours(160);
+		earns = sf1.earns();
+		System.out.printf("Staff Employee: " + sf1.getName() + " earns $" + String.format("%.2f", earns) + " this pay period\n");
+
+		/* Raise is given as %. For example 10% */
+
+		sf1.raise(10);
+		System.out.println("After 10% Raise ....");
+		earns = sf1.earns();
+		System.out.printf("Staff Employee: " + sf1.getName() + " earns $" + String.format("%.2f", earns) + " this pay period\n");
+
+		/* Test Professor pay increase */
+
+		/* Salary is $ per year */
+
+		p1.setSalary(120000);
+		earns = p1.earns();
+
+		System.out.println("\nProfessor: " + p1.getName() + " earns $" + String.format("%.2f", earns) + " this pay period");
+
+		/* Raise is given as %. For example 10% */
+
+		p1.raise(8);
+		System.out.println("After 8% Raise ....");
+		earns = p1.earns();
+		System.out.println("Professor: " + p1.getName() + " earns $" + String.format("%.2f", earns) + " this pay period");
 
 		/*
-		 * Direct test for collisionDetection() inherited method from Student and professor class
+		 * Direct test for collisionDetection() inherited method from Person
 		 * class
 		 */
 
-		System.out.println("Testing collisionDetection method in Student and professor class ...");
-		p3.addCourse(c6);
+		System.out.println("\nTesting collisionDetection method in Person class ...");
+		p3.addCampusCourse(c6);
 		flag = p3.detectConflict(c8);
 		System.out.println("Is there a conflict between " + c6.getName() + " and " + c8.getName() + " ? : " + flag);
 
@@ -203,39 +262,50 @@ public class Driver2
 		
 		
 		/* Print class roster for CS 387 */
+		//M Print online course roster too
 
-		System.out.println("\nThe roster for course " + univ.departmentList.get(1).getCourseList().get(0).getDepartment().getDepartmentName() + univ.departmentList.get(1).getCourseList().get(0).getCourseNumber() + ":");
-		ArrayList<Student> sr1 = univ.departmentList.get(1).getCourseList().get(0).getStudentRoster();
-		for (Student st : sr1) {
+		System.out.println("\nThe roster for course " + univ.departmentList.get(1).getCampusCourseList().get(0).getDepartment().getDepartmentName() + univ.departmentList.get(1).getCampusCourseList().get(0).getCourseNumber() + ":");
+		ArrayList<Person> sr1 = univ.departmentList.get(1).getCampusCourseList().get(0).getStudentRoster();
+		for (Person st : sr1) {
 			System.out.println(st.getName() + " ");
 		}
 		
 		/* Print class roster for CS 372 */
 
-		System.out.println("\nThe roster for course " + univ.departmentList.get(1).getCourseList().get(1).getDepartment().getDepartmentName() + univ.departmentList.get(1).getCourseList().get(1).getCourseNumber() + ":");
-		ArrayList<Student> sr2 = univ.departmentList.get(1).getCourseList().get(1).getStudentRoster();
-		for (Student st : sr2) {
+		System.out.println("\nThe roster for course " + univ.departmentList.get(1).getCampusCourseList().get(1).getDepartment().getDepartmentName() + univ.departmentList.get(1).getCampusCourseList().get(1).getCourseNumber() + ":");
+		ArrayList<Person> sr2 = univ.departmentList.get(1).getCampusCourseList().get(1).getStudentRoster();
+		for (Person st : sr2) {
 			System.out.println(st.getName() + " ");
 		}
 		
 		/* Print student list from university */
 		
 		System.out.println("\nThe list of students in the university:");
-		univ.departmentList.get(0).printStudentList();
-		univ.departmentList.get(1).printStudentList();
+		//univ.departmentList.get(0).printStudentList();
+		//univ.departmentList.get(1).printStudentList();
+		univ.printStudentList();
 		
 		/* Print professor list from university */
 		
 		System.out.println("\nThe list of professors in the university:");
-		univ.departmentList.get(0).printProfessorList();
-		univ.departmentList.get(1).printProfessorList();
+		//univ.departmentList.get(0).printProfessorList();
+		//univ.departmentList.get(1).printProfessorList();
+		univ.printProfessorList();
 		
+		/* Print staff list from university */
+		
+		System.out.println("\nThe list of staff in the university:");
+		//univ.departmentList.get(0).printStaffList();
+		//univ.departmentList.get(1).printStaffList();
+		univ.printStaffList();
 		
 		/* Print course list from university */
 		
 		System.out.println("\nThe list of courses in the university:");
-		univ.departmentList.get(0).printCourseList();
-		univ.departmentList.get(1).printCourseList();
+		//M Print online list of courses
+		//univ.departmentList.get(0).printCourseList();
+		//univ.departmentList.get(1).printCourseList();
+		univ.printCourseList();
 		
 		/* Print student list of CS department */
 		
@@ -248,6 +318,7 @@ public class Driver2
 		univ.departmentList.get(0).printProfessorList();
 
 		/* Print course list of ECE department */
+		//M Print Online course list too
 
 		System.out.println("\nThe course list for department " + univ.departmentList.get(0).getDepartmentName() + ":");
 		univ.departmentList.get(0).printCourseList();
